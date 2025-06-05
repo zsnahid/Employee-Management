67a3593095b9e2f1a22b5bf859b3116febec2a2e
zsnahid <zahidsadmannahid@gmail.com>
2025-06-05 23:10:55 +0600

    added: navbar and footer

---
+++ src/app/(general-layout)/page.tsx
@@ -0,0 +1,11 @@
+import Footer from "@/components/footer";
+import Navbar from "@/components/navbar";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  );
+}
---
+++ src/components/navbar.tsx
@@ -0,0 +1,78 @@
+export default function Navbar() {
+  const navList = [
+    {
+      path: "/dashboard",
+      label: "Dashboard",
+    },
+    {
+      path: "/contact-us",
+      label: "Contact Us",
+    },
+  ];
+
+  return (
+    <header className="bg-white">
+      <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+        <div className="flex h-16 items-center justify-between">
+          <div className="md:flex md:items-center md:gap-12">
+            <a className="block text-teal-600" href="#">
+              <span className="sr-only">Home</span>
+              <p className="font-sans">SyncoHR</p>
+            </a>
+          </div>
+
+          <div className="hidden md:block">
+            <nav aria-label="Global">
+              <ul className="flex items-center gap-6 text-sm">
+                {navList.map((item) => (
+                  <li key={item.label}>
+                    <a href={item.path} className="hover:text-gray-600">{item.label}</a>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+          </div>
+
+          <div className="flex items-center gap-4">
+            <div className="sm:flex sm:gap-4">
+              <a
+                className="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow-sm"
+                href="#"
+              >
+                Login
+              </a>
+
+              <div className="hidden sm:flex">
+                <a
+                  className="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600"
+                  href="#"
+                >
+                  Register
+                </a>
+              </div>
+            </div>
+
+            <div className="block md:hidden">
+              <button className="rounded-sm bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="size-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
